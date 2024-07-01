91a31fc1168596f1975e457a31b1014ffe048018
nnfuad <nnfuad01@gmail.com>
2024-07-01 22:49:31 +0600

    Added py8

---
+++ python8-compandLogicalops.py
@@ -0,0 +1,16 @@
+# mainly 6 relational/comparison operator
+# returns boolean value
+
+print(4 == 3) # false
+print(4 != 4) # false
+print(4 > 1)  # true
+
+# mainly three logical ops
+# returns boolean value
+
+print(1 or 0) #1
+print(4 and 0) #0
+print(4>3 or 3>4) # True
+print(4>3 and 3>4) #False
+print(not 4>=4 ) #False
+print(not 0) #True
