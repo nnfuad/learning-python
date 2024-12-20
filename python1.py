3dd56f90d2bf540ae806c6e2e8af51f12995d24b
nnfuad <nnfuad01@gmail.com>
2024-12-20 15:06:46 +0600

    renewed

--- python1.py
+++ python1.py
@@ -5,3 +5,4 @@ height = 1.75
 print("Hi...I am"+name+". I am "+str(age)+" years old and I'm "+str(height)+"meters tall...")
 
 #print(f"Hi I am {name}. I am {age} years old and I'm {height} meters tall...")
+#print ("Hi...I am",name,".I am ",age," years old.")
